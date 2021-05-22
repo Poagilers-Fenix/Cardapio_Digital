9079b65f27835a75443a210241f9062182074052
larit <larissa.laritt@icloud.com>
2021-05-21 22:53:36 -0300

    Adiciona mudança no modelo lógico e relacional, DDL, DML e DQL.

--- Pasta DB/Scripts/Tabela_Cardap_DDL.sql
+++ Pasta DB/Scripts/Tabela_Cardap_DDL.sql
@@ -16,7 +16,7 @@ CREATE TABLE t_cpp_avaliacao (
     ds_avaliacao        VARCHAR2(300)
 );
 
-ALTER TABLE t_cpp_avaliacao ADD CONSTRAINT t_cpp_avaliacao_pk PRIMARY KEY ( cd_avaliacao );
+ALTER TABLE t_cpp_avaliacao ADD CONSTRAINT pk_cpp_avaliacao PRIMARY KEY ( cd_avaliacao );
 
 CREATE TABLE t_cpp_cliente (
     cd_cliente  NUMBER(5) NOT NULL,
@@ -26,12 +26,12 @@ CREATE TABLE t_cpp_cliente (
 );
 
 COMMENT ON COLUMN t_cpp_cliente.cd_cliente IS
-    'até dez mil';
+    'atÃ© dez mil';
 
 COMMENT ON COLUMN t_cpp_cliente.nr_celular IS
     '(11)922332233';
 
-ALTER TABLE t_cpp_cliente ADD CONSTRAINT t_cpp_cliente_pk PRIMARY KEY ( cd_cliente );
+ALTER TABLE t_cpp_cliente ADD CONSTRAINT pk_cpp_cliente PRIMARY KEY ( cd_cliente );
 
 CREATE TABLE t_cpp_estabelecimento (
     cd_estabelecimento  NUMBER(3) NOT NULL,
@@ -40,7 +40,7 @@ CREATE TABLE t_cpp_estabelecimento (
     ds_cnpj             CHAR(14) NOT NULL
 );
 
-ALTER TABLE t_cpp_estabelecimento ADD CONSTRAINT t_cpp_estabelecimento_pk PRIMARY KEY ( cd_estabelecimento );
+ALTER TABLE t_cpp_estabelecimento ADD CONSTRAINT pk_cpp_estabelecimento PRIMARY KEY ( cd_estabelecimento );
 
 CREATE TABLE t_cpp_gerente (
     cd_gerente   NUMBER(3) NOT NULL,
@@ -50,13 +50,13 @@ CREATE TABLE t_cpp_gerente (
     ds_endereco  VARCHAR2(150) NOT NULL
 );
 
-ALTER TABLE t_cpp_gerente ADD CONSTRAINT t_cpp_gerente_pk PRIMARY KEY ( cd_gerente );
+ALTER TABLE t_cpp_gerente ADD CONSTRAINT pk_cpp_gerente PRIMARY KEY ( cd_gerente );
 
 CREATE TABLE t_cpp_item_cardapio (
     cd_item_cardapio    NUMBER(4) NOT NULL,
     cd_estabelecimento  NUMBER(3) NOT NULL,
     nr_destaque         CHAR(1) NOT NULL,
-    nm_item_pedido      VARCHAR2(50) NOT NULL,
+    nm_item_cardapio    VARCHAR2(50) NOT NULL,
     ds_item_cardapio    VARCHAR2(300) NOT NULL,
     vl_item_cardapio    NUMBER(7, 2) NOT NULL,
     ds_ingrendientes    VARCHAR2(300) NOT NULL,
@@ -65,68 +65,60 @@ CREATE TABLE t_cpp_item_cardapio (
     qt_item_cardapio    NUMBER(3) NOT NULL
 );
 
-ALTER TABLE t_cpp_item_cardapio ADD constraint t_cpp_item_cardapio_ck CHECK ( ds_categoria IN (
-    'PRATO',
-    'BEBIDA',
-    'APERITIVO',
-    'SOBREMESA',
-    'LANCHE'
-))
-;
-ALTER TABLE t_cpp_item_cardapio ADD CONSTRAINT t_cpp_item_cardapio_pk PRIMARY KEY ( cd_item_cardapio );
+ALTER TABLE t_cpp_item_cardapio
+    ADD CONSTRAINT ck_cpp_item_cardapio CHECK ( ds_categoria IN ( 'SOBREMESA', 'PRATO', 'BEBIDA', 'LANCHE', 'APERITIVO' ) );
+
+ALTER TABLE t_cpp_item_cardapio ADD CONSTRAINT pk_cpp_item_cardapio PRIMARY KEY ( cd_item_cardapio );
 
 CREATE TABLE t_cpp_pedido (
     cd_pedido           NUMBER(4) NOT NULL,
     cd_cliente          NUMBER(5) NOT NULL,
     cd_estabelecimento  NUMBER(3) NOT NULL,
     vl_total_pedido     NUMBER(8, 2) NOT NULL,
-    st_pedido           VARCHAR2(15 CHAR) NOT NULL,
+    st_pedido           VARCHAR2(15) NOT NULL,
     hr_pedido           DATE NOT NULL
 );
 
-ALTER TABLE t_cpp_pedido ADD constraint t_cpp_pedido_ck_1 CHECK ( st_pedido IN (
-    'RECEBIDO',
-    'EM PREPARO',
-    'ENTREGUE',
-    'PAGO' ))
-;
-ALTER TABLE t_cpp_pedido ADD CONSTRAINT t_cpp_pedido_pk PRIMARY KEY ( cd_pedido );
+ALTER TABLE t_cpp_pedido
+    ADD CONSTRAINT ck_cpp_pedido CHECK ( st_pedido IN ( 'RECEBIDO', 'EM PREPARO', 'ENTREGUE', 'PAGO' ) );
+
+ALTER TABLE t_cpp_pedido ADD CONSTRAINT pk_cpp_pedido PRIMARY KEY ( cd_pedido );
 
 CREATE TABLE t_cpp_pedido_item_cardapio (
     cd_item_cardapio  NUMBER(4) NOT NULL,
     cd_pedido         NUMBER(4) NOT NULL
 );
 
-ALTER TABLE t_cpp_pedido
-    ADD CONSTRAINT t_cpp_cliente_fk FOREIGN KEY ( cd_cliente )
+ALTER TABLE t_cpp_avaliacao
+    ADD CONSTRAINT fk_cpp_avaliacao_cliente FOREIGN KEY ( cd_cliente )
         REFERENCES t_cpp_cliente ( cd_cliente );
 
 ALTER TABLE t_cpp_avaliacao
-    ADD CONSTRAINT t_cpp_cliente_fkv2 FOREIGN KEY ( cd_cliente )
-        REFERENCES t_cpp_cliente ( cd_cliente );
-
-ALTER TABLE t_cpp_item_cardapio
-    ADD CONSTRAINT t_cpp_estabelecimento_fk FOREIGN KEY ( cd_estabelecimento )
-        REFERENCES t_cpp_estabelecimento ( cd_estabelecimento );
-
-ALTER TABLE t_cpp_avaliacao
-    ADD CONSTRAINT t_cpp_estabelecimento_fkv2 FOREIGN KEY ( cd_estabelecimento )
-        REFERENCES t_cpp_estabelecimento ( cd_estabelecimento );
-
-ALTER TABLE t_cpp_pedido
-    ADD CONSTRAINT t_cpp_estabelecimento_fkv3 FOREIGN KEY ( cd_estabelecimento )
+    ADD CONSTRAINT fk_cpp_avaliacao_estab FOREIGN KEY ( cd_estabelecimento )
         REFERENCES t_cpp_estabelecimento ( cd_estabelecimento );
 
 ALTER TABLE t_cpp_estabelecimento
-    ADD CONSTRAINT t_cpp_gerente_fk FOREIGN KEY ( cd_gerente )
+    ADD CONSTRAINT fk_cpp_estab_gerente FOREIGN KEY ( cd_gerente )
         REFERENCES t_cpp_gerente ( cd_gerente );
 
 ALTER TABLE t_cpp_pedido_item_cardapio
-    ADD CONSTRAINT t_cpp_item_cardapio_fk FOREIGN KEY ( cd_item_cardapio )
+    ADD CONSTRAINT fk_cpp_item_cardapio FOREIGN KEY ( cd_item_cardapio )
         REFERENCES t_cpp_item_cardapio ( cd_item_cardapio );
 
+ALTER TABLE t_cpp_item_cardapio
+    ADD CONSTRAINT fk_cpp_item_cardapio_estab FOREIGN KEY ( cd_estabelecimento )
+        REFERENCES t_cpp_estabelecimento ( cd_estabelecimento );
+
+ALTER TABLE t_cpp_pedido
+    ADD CONSTRAINT fk_cpp_pedido_cliente FOREIGN KEY ( cd_cliente )
+        REFERENCES t_cpp_cliente ( cd_cliente );
+
+ALTER TABLE t_cpp_pedido
+    ADD CONSTRAINT fk_cpp_pedido_estab FOREIGN KEY ( cd_estabelecimento )
+        REFERENCES t_cpp_estabelecimento ( cd_estabelecimento );
+
 ALTER TABLE t_cpp_pedido_item_cardapio
-    ADD CONSTRAINT t_cpp_pedido_fk FOREIGN KEY ( cd_pedido )
+    ADD CONSTRAINT fk_cpp_pedido_item_cardapio FOREIGN KEY ( cd_pedido )
         REFERENCES t_cpp_pedido ( cd_pedido );
 
 
