a93fa6aa23557fdac4b6cf421595641674a0117e
giovannadml <gih_mello@outlook.com>
2021-08-31 11:31:21 -0300

    primeira versão projeto cloud

---
+++ Pasta Cloud&Devops/Cardapp.WebApp/Cardapp.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Cardapp.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cardapp.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Pasta Cloud&Devops/Cardapp.WebApp/Cardapp.WebApp/Controllers/ItemCardapioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cardapp.WebApp.Controllers
+{
+    public class ItemCardapioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
