3863fe29f212f2fbc3dffb7b54390b16c705143f
Henrique <riquenlago36@gmail.com>
2021-09-05 22:26:42 -0300

    chore: create login methods

--- pages/Login/SignUp.js
+++ pages/Login/SignUp.js
@@ -1,76 +1,132 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 
-import Botao from '../../components/Button';
-import InputWithIcon from '../../components/input/InputWithIcon'
+import { getUsers, setUsers } from "../../Storage";
+import InputWithIcon from "../../components/input/InputWithIcon";
 
 export default function SignUp({ navigation }) {
-  return(
+  const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+  const [listUser, setListUser] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const list = await getUsers();
+      if (list !== null && list.length > 0) {
+        setListUser(list);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const handleRegister = () => {
+    if (senha !== confirmarSenha) {
+      Alert.alert("Erro", "As Senhas estão diferentes");
+      return;
+    }
+    if (listUser.filter((e) => e.telefone === telefone).length > 0) {
+      Alert.alert("Erro", "Este telefone já está cadastrado");
+      return;
+    }
+    let users = [...listUser];
+    const usr = {
+      nome,
+      telefone,
+      senha,
+      confirmarSenha,
+    };
+    users.push(usr);
+    setUsers(users);
+    setListUser(users);
+    navigation.navigate("QRCodeReader");
+  };
+
+  return (
     <View style={styles.container}>
-      <Image style={styles.imagem} source={require("../../assets/perfil-cinza.png")} />
+      <Image
+        style={styles.imagem}
+        source={require("../../assets/perfil-cinza.png")}
+      />
       <Text style={styles.titulo}>Cadastro</Text>
 
-      <InputWithIcon title="Nome" icon="account"/>
+      <InputWithIcon title="Nome" icon="account" onChange={setNome} />
 
-      <InputWithIcon title="Telefone" icon="cellphone-android" type="numeric"/>
+      <InputWithIcon
+        title="Telefone"
+        icon="cellphone-android"
+        type="numeric"
+        onChange={setTelefone}
+      />
 
-      <InputWithIcon title="Senha" icon="lock-outline"/>
-      <InputWithIcon title="Confirmar senha" icon="lock-outline"/>
+      <InputWithIcon title="Senha" icon="lock-outline" onChange={setSenha} />
+      <InputWithIcon
+        title="Confirmar senha"
+        icon="lock-outline"
+        onChange={setConfirmarSenha}
+      />
 
       <View style={{ width: 250, marginTop: 30 }}>
-        <Botao 
-          titulo="Entrar" acao={"QRCodeReader"} 
-          navigation={navigation}
-        />
-        <TouchableOpacity style={{alignItems: 'center'}}>
+        <TouchableOpacity onPress={handleRegister}>
+          <Text>Cadastro</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={{ alignItems: "center" }}>
           <Text style={styles.link}>Políticas de Privacidade</Text>
         </TouchableOpacity>
       </View>
-
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
   imagem: {
     width: 100,
-    height: 100
-  }, 
+    height: 100,
+  },
   titulo: {
-    textAlign:'center',
+    textAlign: "center",
     fontSize: 38,
-    color: '#480000',
+    color: "#480000",
     marginBottom: 10,
   },
   label: {
-    color: '#666',
-    fontWeight: 'bold',
+    color: "#666",
+    fontWeight: "bold",
     marginTop: 10,
   },
   input: {
     borderWidth: 2,
-    borderColor: '#aaa',
+    borderColor: "#aaa",
     borderRadius: 5,
     width: 300,
     height: 40,
     marginBottom: 10,
-    alignItems: 'center',
+    alignItems: "center",
     paddingLeft: 5,
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   link: {
-    marginTop: 20, 
-    color: '#800', 
-    borderBottomColor: '#800a', 
-    borderBottomWidth: 1, 
+    marginTop: 20,
+    color: "#800",
+    borderBottomColor: "#800a",
+    borderBottomWidth: 1,
     width: 200,
-    textAlign: 'center',
-    fontSize: 18
-  }
+    textAlign: "center",
+    fontSize: 18,
+  },
 });
