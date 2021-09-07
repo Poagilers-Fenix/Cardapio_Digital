623ae85fddc02a9fca2cf77251f880aae69819aa
Eric-Pessoa <pessoa.eric@gmail.com>
2021-09-07 19:35:05 -0300

    Chore: Insert page, initiation of DB, model

---
+++ Pasta Cloud&Devops/Cardapp.WebApp/Cardapp.WebApp/Models/ItemCardapio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cardapp.WebApp.Models
+{
+    public class ItemCardapio
+    {
+        public string nome { get; set; }
+
+        public string descricao { get; set; }
+
+        public double valor { get; set; }
+
+        public string ingredientes { get; set; }
+
+        public double valorCalorico { get; set; }
+
+        public Boolean destaque { get; set; }
+
+        public string foto { get; set; }
+
+    }
+}
---
+++ Pasta Cloud&Devops/Cardapp.WebApp/Cardapp.WebApp/Repository/Context/DataBaseContext.cs
@@ -0,0 +1,31 @@
+﻿
+using Cardapp.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Oracle.EntityFrameworkCore;
+using System.IO;
+
+namespace Cardapp.WebApp.Repository.Context
+{
+    public class DataBaseContext
+    {
+        public class DataBaseContext : DbContext
+        {
+
+            public DbSet<ItemCardapio> ItemCardapio { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                if (!optionsBuilder.IsConfigured)
+                {
+                    var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+                    optionsBuilder.UseOracle(config.GetConnectionString("FiapSmartCityConnection"));
+                }
+            }
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+
+            }
+        }
+    }
+}
