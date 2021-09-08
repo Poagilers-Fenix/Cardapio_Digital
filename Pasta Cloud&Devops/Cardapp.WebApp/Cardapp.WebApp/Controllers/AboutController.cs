c8d24b866caf6834264d1dd24288688e1f3e2be8
Henrique <riquenlago36@gmail.com>
2021-09-08 11:07:49 -0300

    chore: criação de pag sobre o projeto

---
+++ Pasta Cloud&Devops/Cardapp.WebApp/Cardapp.WebApp/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cardapp.WebApp.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: AboutController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
