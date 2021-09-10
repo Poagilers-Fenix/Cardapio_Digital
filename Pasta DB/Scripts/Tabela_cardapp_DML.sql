f8545113e530fccb478f27209fca792463844850
Eric-Pessoa <pessoa.eric@gmail.com>
2021-09-10 18:00:36 -0300

    fix: mock images

--- Pasta DB/Scripts/Tabela_cardapp_DML.sql
+++ Pasta DB/Scripts/Tabela_cardapp_DML.sql
@@ -55,19 +55,19 @@ insert into t_cpp_categoria values (cdcategoria.nextval, 3, 'Bebida');
  
 
 -- POPULAÇÃO DA TABELA ITEM_CARDAPIO
-INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 1, 1, '1', 'Sorvete de Chocolate', '100g de sorvete sabor chocolate', 5.00, 700, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/chocolate.jpg');
-INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 1, 1, '0', 'Milk Shake de Menta com Chocolate', '150ml de sorvete sabor menta e chocolate', 7.00, 750, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/menta%20chocolate.jpg');
-INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 2, 3, '1', 'Churros de Chocolate', '1 unidade de churros sabor chocolate', 7.00, 850, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/churros%20chocolate.jpg');
-INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 2, 3, '0', 'Churros de Doce de Leite', '1 unidade de churros sabor doce de leite', 7.00, 800, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/churros%20doce%20de%20leite.jpg');
-INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 3, 6, '1', 'Arroz', '100g de arroz do tipo 1', 3.00, 200, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/arroz.jpg');
-INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 3, 6, '0', 'Feijão', '100g de feijão carioca', 4.00, 300, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/feijao.jpg');
-insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 1, 1, '1', 'Milkshake unicórnio','Com MeMs', 8.00, 500, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/milkshake%20unicornio.jpg');
-insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 1, 1, '0', 'Milkshake ovomaltine','O preferido da casa', 12.00, 300, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/ovomaltine.jpg');
-insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 2, 5, '1', 'Cerveja pilsen','A melhor cerveja do MUNDO', 10.00, 43, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/cerveja%20pilsen.jpg');
-insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 2, 4, '0', 'Amendoim sem casca','Um bom aperitivo, talvez o melhor de todos', 2.00, 30, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/amendoim.jpg');
-insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 3, 8, '0', 'Vinho tinto espanhol','Quer ostentar? então toma, ano de 1930', 10000.00, 0, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/vinho%20tinto.jpg');
-insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 3, 6, '1', 'Virada à paulista','Venha comer esse clássico!', 15.00, 400, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/virada%20%C3%A0%20paulista.jpg');
-insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 3, 7, '1', 'Lanche Cheddar Duplo','200g de hambúrguer artesanal e 100g de cheddar,', 20.00, 500, 'https://github.com/Poagilers-Fenix/Cardapio_Digital/blob/main/Imagens/lanche%20cheddar.jpg');
+INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 1, 1, '1', 'Sorvete de Chocolate', '100g de sorvete sabor chocolate', 5.00, 700, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/chocolate.jpg');
+INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 1, 1, '0', 'Milk Shake de Menta com Chocolate', '150ml de sorvete sabor menta e chocolate', 7.00, 750, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/menta%20chocolate.jpg');
+INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 2, 3, '1', 'Churros de Chocolate', '1 unidade de churros sabor chocolate', 7.00, 850, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/churros%20chocolate.jpg');
+INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 2, 3, '0', 'Churros de Doce de Leite', '1 unidade de churros sabor doce de leite', 7.00, 800, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/churros%20doce%20de%20leite.jpg');
+INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 3, 6, '1', 'Arroz', '100g de arroz do tipo 1', 3.00, 200, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/arroz.jpg');
+INSERT INTO t_cpp_item_cardapio VALUES(cditemcardapio.nextval, 3, 6, '0', 'Feijão', '100g de feijão carioca', 4.00, 300, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/feijao.jpg');
+insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 1, 1, '1', 'Milkshake unicórnio','Com MeMs', 8.00, 500, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/milkshake%20unicornio.jpg');
+insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 1, 1, '0', 'Milkshake ovomaltine','O preferido da casa', 12.00, 300, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/ovomaltine.jpg');
+insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 2, 5, '1', 'Cerveja pilsen','A melhor cerveja do MUNDO', 10.00, 43, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/cerveja%20pilsen.jpg');
+insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 2, 4, '0', 'Amendoim sem casca','Um bom aperitivo, talvez o melhor de todos', 2.00, 30, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/amendoim.jpg');
+insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 3, 8, '0', 'Vinho tinto espanhol','Quer ostentar? então toma, ano de 1930', 10000.00, 0, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/vinho%20tinto.jpg');
+insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 3, 6, '1', 'Virada à paulista','Venha comer esse clássico!', 15.00, 400, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/virada%20%C3%A0%20paulista.jpg');
+insert into t_cpp_item_cardapio values(cditemcardapio.nextval, 3, 7, '1', 'Lanche Cheddar Duplo','200g de hambúrguer artesanal e 100g de cheddar,', 20.00, 500, 'https://raw.githubusercontent.com/Poagilers-Fenix/Cardapio_Digital/main/Imagens/lanche%20cheddar.jpg');
 
  
 -- POPULAÇÃO DA TABELA CLIENTE
